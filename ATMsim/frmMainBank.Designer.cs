33cbc479f423cc174c9da0b10480dd740261b4d1
Daniel <daniel-denley@live.co.uk>
2020-04-05 19:01:54 +0100

    Added edit account details

--- ATMsim/frmMainBank.Designer.cs
+++ ATMsim/frmMainBank.Designer.cs
@@ -28,12 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblEditHeader = new System.Windows.Forms.Label();
+            this.lblAddHeader = new System.Windows.Forms.Label();
+            this.lblDeleteHeader = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblEditAccNum = new System.Windows.Forms.Label();
+            this.lblEditBal = new System.Windows.Forms.Label();
+            this.lblEditPin = new System.Windows.Forms.Label();
+            this.txtEditAccNum = new System.Windows.Forms.TextBox();
+            this.txtEditBal = new System.Windows.Forms.TextBox();
+            this.txtEditPin = new System.Windows.Forms.TextBox();
+            this.btnEditSearchAcc = new System.Windows.Forms.Button();
+            this.btnEditUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEditHeader
+            // 
+            this.lblEditHeader.AutoSize = true;
+            this.lblEditHeader.Location = new System.Drawing.Point(74, 29);
+            this.lblEditHeader.Name = "lblEditHeader";
+            this.lblEditHeader.Size = new System.Drawing.Size(106, 13);
+            this.lblEditHeader.TabIndex = 0;
+            this.lblEditHeader.Text = "Edit Account Details:";
+            // 
+            // lblAddHeader
+            // 
+            this.lblAddHeader.AutoSize = true;
+            this.lblAddHeader.Location = new System.Drawing.Point(74, 183);
+            this.lblAddHeader.Name = "lblAddHeader";
+            this.lblAddHeader.Size = new System.Drawing.Size(72, 13);
+            this.lblAddHeader.TabIndex = 1;
+            this.lblAddHeader.Text = "Add Account:";
+            // 
+            // lblDeleteHeader
+            // 
+            this.lblDeleteHeader.AutoSize = true;
+            this.lblDeleteHeader.Location = new System.Drawing.Point(59, 334);
+            this.lblDeleteHeader.Name = "lblDeleteHeader";
+            this.lblDeleteHeader.Size = new System.Drawing.Size(84, 13);
+            this.lblDeleteHeader.TabIndex = 2;
+            this.lblDeleteHeader.Text = "Delete Account:";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(672, 404);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(116, 34);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Back";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblEditAccNum
+            // 
+            this.lblEditAccNum.AutoSize = true;
+            this.lblEditAccNum.Location = new System.Drawing.Point(74, 66);
+            this.lblEditAccNum.Name = "lblEditAccNum";
+            this.lblEditAccNum.Size = new System.Drawing.Size(90, 13);
+            this.lblEditAccNum.TabIndex = 4;
+            this.lblEditAccNum.Text = "Account Number:";
+            // 
+            // lblEditBal
+            // 
+            this.lblEditBal.AutoSize = true;
+            this.lblEditBal.Location = new System.Drawing.Point(74, 102);
+            this.lblEditBal.Name = "lblEditBal";
+            this.lblEditBal.Size = new System.Drawing.Size(49, 13);
+            this.lblEditBal.TabIndex = 5;
+            this.lblEditBal.Text = "Balance:";
+            // 
+            // lblEditPin
+            // 
+            this.lblEditPin.AutoSize = true;
+            this.lblEditPin.Location = new System.Drawing.Point(74, 134);
+            this.lblEditPin.Name = "lblEditPin";
+            this.lblEditPin.Size = new System.Drawing.Size(25, 13);
+            this.lblEditPin.TabIndex = 6;
+            this.lblEditPin.Text = "Pin:";
+            // 
+            // txtEditAccNum
+            // 
+            this.txtEditAccNum.Location = new System.Drawing.Point(211, 63);
+            this.txtEditAccNum.MaxLength = 6;
+            this.txtEditAccNum.Name = "txtEditAccNum";
+            this.txtEditAccNum.Size = new System.Drawing.Size(100, 20);
+            this.txtEditAccNum.TabIndex = 7;
+            this.txtEditAccNum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtEditAccNum_KeyPress);
+            // 
+            // txtEditBal
+            // 
+            this.txtEditBal.Location = new System.Drawing.Point(211, 95);
+            this.txtEditBal.Name = "txtEditBal";
+            this.txtEditBal.Size = new System.Drawing.Size(100, 20);
+            this.txtEditBal.TabIndex = 8;
+            this.txtEditBal.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtEditBal_KeyPress);
+            // 
+            // txtEditPin
+            // 
+            this.txtEditPin.Location = new System.Drawing.Point(211, 131);
+            this.txtEditPin.MaxLength = 4;
+            this.txtEditPin.Name = "txtEditPin";
+            this.txtEditPin.Size = new System.Drawing.Size(100, 20);
+            this.txtEditPin.TabIndex = 9;
+            this.txtEditPin.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtEditPin_KeyPress);
+            // 
+            // btnEditSearchAcc
+            // 
+            this.btnEditSearchAcc.Location = new System.Drawing.Point(328, 61);
+            this.btnEditSearchAcc.Name = "btnEditSearchAcc";
+            this.btnEditSearchAcc.Size = new System.Drawing.Size(105, 23);
+            this.btnEditSearchAcc.TabIndex = 10;
+            this.btnEditSearchAcc.Text = "Find Account";
+            this.btnEditSearchAcc.UseVisualStyleBackColor = true;
+            this.btnEditSearchAcc.Click += new System.EventHandler(this.btnEditSearchAcc_Click);
+            // 
+            // btnEditUpdate
+            // 
+            this.btnEditUpdate.Location = new System.Drawing.Point(328, 129);
+            this.btnEditUpdate.Name = "btnEditUpdate";
+            this.btnEditUpdate.Size = new System.Drawing.Size(105, 23);
+            this.btnEditUpdate.TabIndex = 11;
+            this.btnEditUpdate.Text = "Update Account";
+            this.btnEditUpdate.UseVisualStyleBackColor = true;
+            this.btnEditUpdate.Click += new System.EventHandler(this.btnEditUpdate_Click);
+            // 
+            // frmMainBank
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEditUpdate);
+            this.Controls.Add(this.btnEditSearchAcc);
+            this.Controls.Add(this.txtEditPin);
+            this.Controls.Add(this.txtEditBal);
+            this.Controls.Add(this.txtEditAccNum);
+            this.Controls.Add(this.lblEditPin);
+            this.Controls.Add(this.lblEditBal);
+            this.Controls.Add(this.lblEditAccNum);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblDeleteHeader);
+            this.Controls.Add(this.lblAddHeader);
+            this.Controls.Add(this.lblEditHeader);
+            this.Name = "frmMainBank";
             this.Text = "frmMainBank";
+            this.Load += new System.EventHandler(this.frmMainBank_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblEditHeader;
+        private System.Windows.Forms.Label lblAddHeader;
+        private System.Windows.Forms.Label lblDeleteHeader;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblEditAccNum;
+        private System.Windows.Forms.Label lblEditBal;
+        private System.Windows.Forms.Label lblEditPin;
+        private System.Windows.Forms.TextBox txtEditAccNum;
+        private System.Windows.Forms.TextBox txtEditBal;
+        private System.Windows.Forms.TextBox txtEditPin;
+        private System.Windows.Forms.Button btnEditSearchAcc;
+        private System.Windows.Forms.Button btnEditUpdate;
     }
 }
--- ATMsim/frmMainBank.cs
+++ ATMsim/frmMainBank.cs
@@ -12,9 +12,96 @@ namespace ATMsim
 {
     public partial class frmMainBank : Form
     {
-        public frmMainBank()
+
+        int accNumInput;
+        int accSelectedBal;
+        int accSelectedPin;
+        bool accFound;
+        int accIndex;
+
+        Form1 frm1;
+        public frmMainBank(Form1 f)
         {
             InitializeComponent();
+            frm1 = f;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void frmMainBank_Load(object sender, EventArgs e)
+        {
+            txtEditBal.Enabled = false;
+            txtEditPin.Enabled = false;
+            btnEditUpdate.Enabled = false;
+        }
+
+        private void txtEditAccNum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtEditBal_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private void txtEditPin_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnEditSearchAcc_Click(object sender, EventArgs e)
+        {
+            accNumInput = Int32.Parse(txtEditAccNum.Text);
+            accFound = false;
+            for (int i = 0; i < frm1.ac.Length; i++)
+            {
+                if (accNumInput == frm1.ac[i].getAccountNum())
+                {
+                    accSelectedBal = frm1.ac[i].getBalance();
+                    accSelectedPin = frm1.ac[i].getPin();
+                    txtEditBal.Text = (accSelectedBal).ToString();
+                    txtEditPin.Text = (accSelectedPin).ToString();
+                    txtEditBal.Enabled = true;
+                    txtEditPin.Enabled = true;
+                    btnEditUpdate.Enabled = true;
+                    accFound = true;
+                    accIndex = i;
+                }
+                if (accFound == false)
+                {
+                    txtEditBal.Text = "";
+                    txtEditPin.Text = "";
+                    txtEditBal.Enabled = false;
+                    txtEditPin.Enabled = false;
+                    btnEditUpdate.Enabled = false;
+                }
+            }
+        }
+
+        private void btnEditUpdate_Click(object sender, EventArgs e)
+        {
+            int bal = Int32.Parse(txtEditBal.Text);
+            int pin = Int32.Parse(txtEditPin.Text);
+            frm1.ac[accIndex].setBalance(bal);
+            frm1.ac[accIndex].setPin(pin);
+            accSelectedBal = frm1.ac[accIndex].getBalance();
+            accSelectedPin = frm1.ac[accIndex].getPin();
+            txtEditBal.Text = (accSelectedBal).ToString();
+            txtEditPin.Text = (accSelectedPin).ToString();
         }
     }
 }
